0b48bb2fd9913f490281324152a7665ad9974f9d
Michael Santos <michael.santos@gmail.com>
2018-01-30 08:43:52 -0500

    capsicum: allow reading stdin

--- sandbox_capsicum.c
+++ sandbox_capsicum.c
@@ -28,12 +28,11 @@ sandbox_init()
     struct rlimit rl = {0};
     cap_rights_t policy_read;
     cap_rights_t policy_write;
-    int fd = -1;
 
     if (setrlimit(RLIMIT_NPROC, &rl) < 0)
       return -1;
 
-    (void)cap_rights_init(&policy_read);
+    (void)cap_rights_init(&policy_read, CAP_READ);
     (void)cap_rights_init(&policy_write, CAP_WRITE, CAP_FSTAT);
 
     if (cap_rights_limit(STDIN_FILENO, &policy_read) < 0)
